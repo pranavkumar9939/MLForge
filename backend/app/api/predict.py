16cc95ea34479d82887d8a5e849f9dac2ba92720
pranavkumar9939 <pranavvicky9939@gmail.com>
2026-07-20 21:32:44 +0530

    Implement model saving, loading, and prediction pipeline

--- backend/app/api/predict.py
+++ backend/app/api/predict.py
@@ -1,9 +1,26 @@
+import pandas as pd
+
 from fastapi import APIRouter
 from app.schemas.prediction import PredictionRequest
+from app.services.persistence.model_loader import load_saved_model
 
 router = APIRouter(prefix = "/predict", tags = ["Predict"])
 
 @router.post("/")
 def get_prediction(request: PredictionRequest):
 
-    return request
+    artifacts = load_saved_model()
+
+    input_df = pd.DataFrame([request.features])
+
+    X = artifacts["pipeline"].transform(input_df)
+    prediction = artifacts["model"].predict(X)[0]
+
+    if artifacts["label_encoder"] is not None:
+        prediction = artifacts["label_encoder"].inverse_transform([prediction])[0]
+
+    
+
+    return {
+        "prediction": prediction
+    }
